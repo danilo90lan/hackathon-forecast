6d12484f72836582ecbffb74e9ab9625a73e239f
xfinitymusic <xfinitymusic@gmail.com>
2024-12-07 02:59:34 +0000

    Create basic site scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+    <Navbar bg='dark' variant='dark' expand='lg' sticky='top'>
+      <Container>
+        <Navbar.Brand as={Link} to='/'>
+          Hackathon Project
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link as={Link} to='/'>
+              Home
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  </Router>
+  <ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+  </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap"
+
+const Home = () => {
+    return(
+        <Container className='text-center'>
+            <h1>Home</h1>
+        </Container>
+        
+    )
+}
+
+export default Home
