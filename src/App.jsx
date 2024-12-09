133326875b196ddd8b01754471b4ed0c899dcda7
finneh4249 <ethancornwill@outlook.com>
2024-12-09 21:20:07 +0000

    Add savedCitiesContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,44 @@ import { Container, Navbar, Nav } from 'react-bootstrap'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import Home from './pages/Home'
+import 'react-toastify/dist/ReactToastify.css'
+import SavedCitiesProvider from './context/SavedCitiesContext'
 
 function App() {
 
   return (
-    <>
-    <Router>
-    <Navbar bg='dark' variant='dark' expand='lg' sticky='top'>
-      <Container>
-        <Navbar.Brand as={Link} to='/'>
-          WeatherWise
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link as={Link} to='/'>
-              Home
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
-  </Router>
-  <ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-  </>
+    <SavedCitiesProvider>
+      <Router>
+        <Navbar bg='dark' variant='dark' expand='lg' sticky='top'>
+          <Container>
+            <Navbar.Brand as={Link} to='/'>
+              WeatherWise
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto'>
+                <Nav.Link as={Link} to='/'>
+                  Home
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position='top-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </SavedCitiesProvider>
   )
 }
 
---
+++ src/context/SavedCitiesContext.jsx
@@ -0,0 +1,32 @@
+import {createContext, useContext} from 'react'
+import {useLocalStorage} from 'react-use'
+import PropTypes from 'prop-types'
+import {toast} from 'react-toastify'
+
+const SavedCitiesContext = createContext()
+
+export default function SavedCitiesProvider ({children}) {
+    const [savedCities, setSavedCities] = useLocalStorage('savedCities', [])
+
+    const addToSavedCities = (city) => {
+        if (!savedCities.find((item) => item.id === city.id)) {
+          setSavedCities([...savedCities, city])
+          toast.success(`${city.name} has been added to your saved cities!`)
+        } else {
+          toast.info(`${city.name} is already in your saved cities.`)
+        }
+      }
+      return (
+        <SavedCitiesContext.Provider value={{ savedCities, addToSavedCities }}>
+          {children}
+        </SavedCitiesContext.Provider>
+      )
+    }
+    
+    SavedCitiesProvider.propTypes = {
+      children: PropTypes.node.isRequired
+    }
+    
+    export function useSavedCities() {
+      return useContext(SavedCitiesContext)
+    }
