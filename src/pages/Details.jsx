1abed8601f4c61741f893678fc30f31db349ffde
finneh4249 <ethancornwill@outlook.com>
2024-12-12 10:29:14 +0000

    Rudimentary details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,11 +1,23 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import cities from '../data/cities.json'
+import SevenDayForecast from './SevenDayForecast'
 
 const Details = () => {
     const {id} = useParams()
+    const cityName = cities.filter((city) => city.id == id)
+    console.log(cityName)
 
     useEffect(() => {
     })
+
+    return (
+        <div>
+            <h1>{cityName[0].name}</h1>
+            <SevenDayForecast cityName={cityName[0].name} />
+        </div>
+        
+    )
 }
 
 export default Details
