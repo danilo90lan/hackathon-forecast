e41da87523b7cc4ba900881561b8f636e886710a
finneh4249 <ethancornwill@outlook.com>
2024-12-12 09:56:56 +0000

    Improve styling

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import './DayCard.css'
 
 const DayCard = ({ date, weekday, icon, condition, maxTemp, minTemp, rainPercentage }) => {
     return (
-        <Card className="h-100 rounded-3 shadow-sm bg-white">
-            <Card.Body className="d-flex flex-column justify-content-between">
-                <div className="d-flex justify-content-between align-items-center">
-                    <h5 className="mb-0">{weekday}</h5>
-                    <h5 className="mb-0">{date}</h5>
-                </div>
-                <img
+        <Card className='day-card'>
+            <Card.Body className='day-card-body'>
+            <p className='max-temp'><b>{maxTemp}°</b></p>
+                <p className='min-temp'>{minTemp}°</p>
+               
+                <img 
                     src={icon}
                     alt={condition}
-                    className="my-2"
                     style={{ width: '60px', height: '60px' }}
                 />
-                <p>{condition}</p>
-                <p><strong>Max Temp:</strong> {maxTemp}°C</p>
-                <p><strong>Min Temp:</strong> {minTemp}°C</p>
-                <p><strong>Chance of Rain:</strong> {rainPercentage}</p>
+                {/* <p>{condition}</p> */}
+                
+                <p className='rain-percent'><b>{rainPercentage}</b></p>
+                <h3>{date === new Date().toISOString().split('T')[0] ? 'Today' : weekday}</h3>
             </Card.Body>
         </Card>
     );
