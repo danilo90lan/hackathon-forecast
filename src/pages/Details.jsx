0dc22e9a38be540c54d750ba70c7dd2899b14dfd
finneh4249 <ethancornwill@outlook.com>
2024-12-13 03:48:32 +0000

    Completely refactor Details page to display the details for the city using the data stored in the context

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,20 +1,39 @@
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import cities from '../data/cities.json'
 import SevenDayForecast from '../components/SevenDayForecast'
+import HourlyForecast from '../components/HourlyForecast'
+import {Container} from 'react-bootstrap'
+import { useWeather } from '../context/WeatherContext'
 
 const Details = () => {
     const {id} = useParams()
-    const cityName = cities.filter((city) => city.id == id)
-    console.log(cityName)
-
-    useEffect(() => {
-    })
-
+    const cityName = cities.filter((city) => city.id == id)[0].name
+    const navigate = useNavigate();
+    const {selectedCityForecast} = useWeather()
+    const forecast = selectedCityForecast
+    console.log(forecast)
     return (
-        <div>
-            <h1>{cityName[0].name}</h1>
-            <SevenDayForecast cityName={cityName[0].name} />
+        <div id='hero'>
+            <Container className='text-center margin-xy-1 blurred-bg '>
+            <button onClick={() => navigate(-1)}>
+                Go Back
+            </button>
+            <h1>{cityName}</h1>
+            <Container className='half-width text-center margin-xy-1 blurred-bg dark-text'>
+                <p>{forecast?.current?.condition?.text}</p>
+                <h1>{(forecast?.current?.temp_c).toFixed(0)}°</h1> <img
+                            src={forecast?.current?.condition?.icon}
+                            alt={forecast?.current?.condition?.text}
+                            style={{ width: '60px', height: '60px' }}
+                        />
+                <p>Feels like {(forecast?.current?.feelslike_c).toFixed(0)}°</p>
+                <p>High {(forecast?.forecast?.forecastday?.[0]?.day?.maxtemp_c).toFixed(0)}° - Low {(forecast?.forecast?.forecastday?.[0]?.day?.mintemp_c).toFixed(0)}°</p>
+            </Container>
+            <HourlyForecast cityName={cityName} forecast={forecast} />
+            <SevenDayForecast cityName={cityName} forecast={forecast}/>
+            
+            </Container>
         </div>
         
     )
