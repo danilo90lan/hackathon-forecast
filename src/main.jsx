3ab4c70feb9fcdf88c63d1b173b8941fe2516d09
Ethan Cornwill <ethancornwill@outlook.com>
2024-12-14 02:36:36 +1100

    Add ErrorBoundary component for error handling in the application

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './assets/css/styles.css'
+import ErrorBoundary from './context/ErrorBoundary'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ErrorBoundary>
     <App />
+    </ErrorBoundary>
   </StrictMode>
 )
